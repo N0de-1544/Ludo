c877680340c1f31ae0fabcb13525a0871cfe888c
Art <artur.pollack@gmail.com>
2023-01-23 22:40:09 +1000

    Now Pieces are going in circles. To Do list: -Make them 'eat' each other ( send themselves back to 'respawn point') -Make them go into 'home' section (changes in move method) -Overlapping, issues: You can't see overlapping pieces You can't choose which piece to move And I think that all I need to do in core mechanics (or not). Other important things: Show whose turn it is. Add some sounds, on win do something, starter screen (settings, textures, sounds, ect.) and ect.

--- playersandgame.py
+++ playersandgame.py
@@ -1,5 +1,3 @@
-from random import randint
-
 imboard = [['*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
            ['*', '*', '*', '*', '*', '*', '*', ' ', ' ', ' ', '*', '*', '*', '*', '*', '*', '*'],
            ['*', '*', ' ', '*', '*', ' ', '*', ' ', '_', ' ', '*', ' ', '*', '*', ' ', '*', '*'],
@@ -18,9 +16,10 @@ imboard = [['*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'
            ['*', '*', '*', '*', '*', '*', '*', ' ', ' ', ' ', '*', '*', '*', '*', '*', '*', '*'],
            ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*']]
 
+order = ['b', 'r', 'g', 'y']
+colorresppos = {'b': (7, 14), 'r': (14, 9), 'g': (9, 2), 'y': (2, 7)}
+whoseturn = 3
 
-def diceroll():
-    return randint(1, 6)
 
 class Piece:
     def __init__(self, startpos, color):
@@ -33,39 +32,48 @@ class Piece:
 
     def activate(self):
         self.active = True
+        self.y, self.x = colorresppos[self.color]
+        print('done!' + self.color)
 
     def move(self, tiles):
-        if self.active is False:
+        if self.active is False and tiles < 6:
             return None
+        if self.active is False and tiles == 6:
+            return self.activate()
         for i in range(tiles):
-            if self.x - 7 > 0:
-                if imboard[self.x][self.y - 1] == ' ':
-                    self.y -= 1
-                elif self.y - 7 < 0:
-                    if imboard[self.x + 1][self.y] == ' ':
-                        self.x += 1
-                elif self.y - 7 > 0:
-                    if imboard[self.x - 1][self.y] == ' ':
-                        self.x -= 1
-            elif self.x - 7 < 0:
-                if imboard[self.x][self.y + 1] == ' ':
+            if self.x - 8 >= 0:
+                if imboard[self.y + 1][self.x] == ' ':
                     self.y += 1
-                elif self.y - 7 < 0:
-                    if imboard[self.x + 1][self.y] == ' ':
-                        self.x += 1
-                elif self.y - 7 > 0:
-                    if imboard[self.x - 1][self.y] == ' ':
-                        self.x -= 1
+                else:
+                    if self.y - 8 > 0:
+                        if imboard[self.y][self.x - 1] == ' ':
+                            self.x -= 1
+                    if self.y - 8 < 0:
+                        if imboard[self.y][self.x + 1] == ' ':
+                            self.x += 1
+            if self.x - 8 < 0:
+                if imboard[self.y - 1][self.x] == ' ':
+                    self.y -= 1
+                else:
+                    if self.y - 8 > 0:
+                        if imboard[self.y][self.x - 1] == ' ':
+                            self.x -= 1
+                    if self.y - 8 < 0:
+                        if imboard[self.y][self.x + 1] == ' ':
+                            self.x += 1
             self.gone_through += 1
 
-    def getpiecestate(self):
+    def getpar(self):
         return self.y, self.x, self.color
+
+    def getcolor(self):
+        return self.color
+
+    def getcoords(self):
+        return self.y, self.x
 
 
 players = [Piece((11, 2), 'g'), Piece((11, 5), 'g'), Piece((14, 2), 'g'), Piece((14, 5), 'g'),
            Piece((2, 2), 'y'), Piece((2, 5), 'y'), Piece((5, 2), 'y'), Piece((5, 5), 'y'),
            Piece((2, 11), 'b'), Piece((2, 14), 'b'), Piece((5, 11), 'b'), Piece((5, 14), 'b'),
            Piece((11, 11), 'r'), Piece((11, 14), 'r'), Piece((14, 11), 'r'), Piece((14, 14), 'r')]
-
-
-
